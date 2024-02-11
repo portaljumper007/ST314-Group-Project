3813cc46709b50de80c8aeacc8bc0c0a92290d7c
tristan-bhandal <t.a.bhandal@lse.ac.uk>
2024-02-11 16:50:23 +0000

    Create Coursework1.R

---
+++ Coursework1.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(expss)
+immigration_data <- read.csv(file="immigration.csv")
+head(immigration_data)
+
+## Question 1
+
+# making 'econact' a factor
+class(immigration_data$econact)
+immigration_data <- apply_labels(immigration_data, econact=c("Employed"= 1, "Education"= 2, "Unemployed"= 3, "OutLabourMarket"= 4))
+immigration_data$econact <- as.factor(immigration_data$econact)
+class(immigration_data$econact)
+
+# making 'female' a factor
+class(immigration_data$female)
+immigration_data$female <- as.factor(immigration_data$female)
+class(immigration_data$female)
+
+# making 'partner' a factor
+class(immigration_data$partner)
+immigration_data$partner <- as.factor(immigration_data$partner)
+class(immigration_data$partner)
+
+nrow(immigration_data) # there are 33706 observations
+length(unique(immigration_data$country)) # there are 27 countries
+
+# new dataset for all countries
+immigration_data.c <- immigration_data %>%
+  group_by(country) %>%
+  summarise(country = first(country), nind = n(), mean.zimmatt = mean(zimmatt), c_zgni = first(c_zgni), c_ltunemp = first(c_ltunemp), c_znetmig = first(c_znetmig))
+immigration_data.c$mean.zimmatt <- round(immigration_data.c$mean.zimmatt, digits = 2)
+print(immigration_data.c, n = 27)
+
+# histograms for zimmatt
+hist(immigration_data$zimmatt)
+hist(immigration_data.c$mean.zimmatt)
+
+# table 
+count <- (table(immigration_data$econact))
+percent <- round(prop.table(table(immigration_data$econact)) * 100, digits = 1)
+rbind(count, percent)
