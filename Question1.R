0d0bfc713597b5646956940da394a994912b68d3
tristan-bhandal <t.a.bhandal@lse.ac.uk>
2024-02-17 17:39:12 +0000

    Update Question1.R

--- Question1.R
+++ Question1.R
@@ -2,11 +2,14 @@ library(dplyr)
 library(expss)
 library(lme4)
 library(sjstats)
+library(ggplot2)
 
 immigration_data <- read.csv(file="immigration.csv")
 head(immigration_data)
 
 ## Question 1
+
+## Exploratory Analysis
 
 # making 'econact' a factor
 class(immigration_data$econact)
@@ -32,6 +35,15 @@ sd(immigration_data$zimmatt) # zimmatt standard deviation
 mean(immigration_data$eduyrs, na.rm=TRUE) # education mean
 (table(immigration_data$female)["0"])/(table(immigration_data$female)["0"] + table(immigration_data$female)["1"]) * 100 # percentage of males
 
+# labour market country breakdown ('econact')
+countries_labour_market <- immigration_data %>%
+  group_by(country) %>%
+  summarise(Employed_percent = round(sum(econact == "Employed", na.rm = TRUE) / n() * 100, 1),
+    Education_percent = round(sum(econact == "Education", na.rm = TRUE) / n() * 100, 1),
+    Unemployed_percent = round(sum(econact == "Unemployed", na.rm = TRUE) / n() * 100, 1),
+    OutLabourMarket_percent = round(sum(econact == "OutLabourMarket", na.rm = TRUE) / n() * 100, 1), .groups = 'drop')
+countries_labour_market %>% arrange(desc(Employed_percent)) # most employed people % countries in order
+countries_labour_market %>% arrange(desc(Unemployed_percent)) # most unemployed people % countries in order
 
 # new dataset for all countries
 immigration_data.c <- immigration_data %>%
@@ -40,22 +52,27 @@ immigration_data.c <- immigration_data %>%
 immigration_data.c$mean.zimmatt <- round(immigration_data.c$mean.zimmatt, digits = 2)
 print(immigration_data.c, n = 27)
 
+## Graphs
+
 # histograms for zimmatt
 hist(immigration_data$zimmatt)
 hist(immigration_data.c$mean.zimmatt)
 
+# countries' attitudes towards immigration
+ggplot(immigration_data.c, aes(x=reorder(country, mean.zimmatt), y=mean.zimmatt)) + geom_bar(stat="identity") +
+  labs(title="Mean Attitudes Towards Immigration by country (standardised)", x="Country", y="Mean Attitude Towards Immigration (standardised)")
 
-# labour market country breakdown ('econact')
-countries_labour_market <- immigration_data %>%
-  group_by(country) %>%
-  summarise(Employed_percent = round(sum(econact == "Employed", na.rm = TRUE) / n() * 100, 1),
-    Education_percent = round(sum(econact == "Education", na.rm = TRUE) / n() * 100, 1),
-    Unemployed_percent = round(sum(econact == "Unemployed", na.rm = TRUE) / n() * 100, 1),
-    OutLabourMarket_percent = round(sum(econact == "OutLabourMarket", na.rm = TRUE) / n() * 100, 1), .groups = 'drop')
-countries_labour_market
+# GNI per capita across countries
+ggplot(immigration_data.c, aes(x=reorder(country, c_zgni), y=c_zgni)) + geom_bar(stat="identity") +
+  labs(title="Gross National Income per capita by country", x="Country", y="GNI per capita (standardised)")
 
+# long term unemployed proportions across countries
+ggplot(immigration_data.c, aes(x=reorder(country, c_ltunemp), y=c_ltunemp)) + geom_bar(stat="identity") +
+  labs(title="Proportion of long-term unemployment by country", x="Country", y="Proportion of Long-term unemployment")
 
-
+# crude net migration rate across countries 
+ggplot(immigration_data.c, aes(x=reorder(country, c_znetmig), y=c_znetmig)) + geom_bar(stat="identity") +
+  labs(title="Crude Net Migration Rate by country", x="Country", y="Crude Net Migration Rate (standardised)")
 
 
 
